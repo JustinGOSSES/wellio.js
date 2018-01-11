95ba79b2104128e1843717fef8a81b69876ec4d5
Justin Gosses <jgosses82@gmail.com>
2018-01-11 00:08:26 -0600

    cleaned up wellio.js a bit

--- js/call_plots.js
+++ js/call_plots.js
@@ -9,7 +9,7 @@ function makePlot(single_curve,div,height,width,domain_x,domain_y){
 	// g3.log(b2,a.data).draw()
 	// console.log('2 single_curve = ',single_curve)
 
-	var b2=g3.plot("#log_plot_div").height(1600).width(200).xDomain([0,500]).yDomain([0,single_curve.length]).draw();
+	var b2=g3.plot("#log_plot_div").height(600).width(200).xDomain([0,300]).yDomain([0,single_curve.length]).draw();
 	g3.log(b2,single_curve).draw()
 }
 
--- js/wellio.js
+++ js/wellio.js
@@ -40,7 +40,6 @@ function las2json(onelas){
 	var well_info_obj = {"MNEM":"","UNIT":"","DATA":"","DESCRIPTION OF MNEMONIC 1":"","DESCRIPTION OF MNEMONIC 2":""};
 	var curve_info_obj = {"MNEM":"","UNIT":"","ERCB CURVE CODE":"","CURVE DESCRIPTION 1":"","CURVE DESCRIPTION 2":""};
 	var param_info_obj = {"MNEM":"","UNIT":"","DATA":"","DESCRIPTION OF MNEMONIC 1":"","DESCRIPTION OF MNEMONIC 2":""};
-	
 	//// The file is read as a txt file. It will first be split into seperate strings based on "~"
 	var split1 = onelas.split("~");
 	var vers_str = split1[1];
@@ -57,7 +56,6 @@ function las2json(onelas){
 		var vers_line_1half_array = vers_line_1half.split(".")
 		ver_info_obj["MNEM"] = vers_line_1half_array[0]
 		var unit_and_data = vers_line_1half_array.slice(1,vers_line_1half_array.length);
-		// console.log("0 unit_and_data = ",unit_and_data);
 		var unit_and_data_str = "                        ";
 		if (unit_and_data.length > 1){
 			unit_and_data_str = unit_and_data[0].toString()+"."+unit_and_data[1].toString();
@@ -65,14 +63,10 @@ function las2json(onelas){
 		else{
 			unit_and_data_str = unit_and_data.toString()
 		}
-		console.log("1 unit_and_data_str = ",unit_and_data_str);
 		var unit = unit_and_data_str[0,5].trim();
 		var data = unit_and_data_str.substring(5,unit_and_data_str.length).trim();
-		// console.log("unit = ",unit);
-		// console.log("data = ",data);
 		ver_info_obj["DATA"] = data
   		ver_info_obj["UNIT"] = unit
-  		// console.log("ver_info_obj['UNIT'] = ",ver_info_obj["UNIT"]);
   		//// 
   		if(arrayString.split(":")[1].indexOf("-") !== -1){
   			ver_info_obj["DESCRIPTION OF MNEMONIC 1"] = arrayString.split(":")[1].split("-")[0].trim()
@@ -88,7 +82,6 @@ function las2json(onelas){
 	lasjson["VERSION INFORMATION"]["VERS"] = splitLineofType1(Object.assign({}, ver_info_obj),vers_line);
 	//// Working with PARAMETER INFORMATION block second by splitting it by newline into an array and taking items after 0,1,2 or [3:]
 	var param_line_array = param_info_str.split("\n").slice(3,);
-	console.log("0 param_line_array =",param_line_array)
 	for(i = 0; i < param_line_array.length; i++){
 		//// create one object for parameter line
 		if(param_line_array[i] != ""){
@@ -98,7 +91,6 @@ function las2json(onelas){
 	}
 	//// Working with CURVE INFORMATION BLOCK second by splitting it by newline into an array and taking items after 0,1,2 or [3:]
 	var curve_line_array = curve_info_str.split("\n").slice(3,);
-	console.log("0 curve_line_array =",curve_line_array)
 	for(i = 0; i < curve_line_array.length; i++){
 		//// create one object for parameter line
 		if(curve_line_array[i] != ""){
@@ -108,7 +100,6 @@ function las2json(onelas){
 	}
 	//// Working with WELL INFORMATION BLOCK second by splitting it by newline into an array and taking items after 0,1,2 or [3:]
 	var well_line_array = well_info_str.split("\n").slice(3,);
-	console.log("0 curve_line_array =",well_line_array)
 	for(i = 0; i < well_line_array.length; i++){
 		if(well_line_array[i].includes("Generated")){
 			lasjson["WELL INFORMATION BLOCK"]["GENERATED"] = well_line_array[i].replace("\r","").replace("\t"," ").replace("#","")
@@ -130,7 +121,6 @@ function las2json(onelas){
 	var curve_names_array = [];
 	var curve_names_array_holder = [];
 	if(curve_str_array[0][0] === "A"){
-		// curve_str_array[0].remove("  "," ").replace("   ","")
 		curve_names_array = curve_str_array[0].split(" ")
 		console.log("0 curve_names_array = ",curve_names_array)
 		curve_names_array = curve_names_array.slice(1,curve_names_array.length);
@@ -141,35 +131,19 @@ function las2json(onelas){
 				lasjson["CURVES"][curve_names_array[i]] = []
 			}
 		}
-		console.log("0 curve_names_array_holder = ",curve_names_array_holder)
-		console.log("1 curve_names_array = ",curve_names_array)
-		console.log("2 lasjson['CURVES'] = ",lasjson["CURVES"])
 	}
 	else{console.log("Couldn't find curve names above curves in LAS, check formatting!")}
 	//// start at position 1 instead of 0 is to avoid the curve names
 	for(j = 1; j < curve_str_array.length-1; j++){
 		var curve_data_line_array = curve_str_array[j].split(" ");
-		// console.log("3 curve_data_line_array[j] = ",curve_data_line_array)
 		var counter_of_curve_names = 0;
 		for(k = 0; k < curve_data_line_array.length; k++){
-			if(curve_data_line_array[k] !== ""){
-				// console.log("4 curve_data_line_array[k] = ",curve_data_line_array[k]);
-				// console.log("5a counter_of_curve_names = ",counter_of_curve_names)
-				// console.log("curve_names_array_holder = ",curve_names_array_holder)
-				// console.log("lasjson['CURVES'] = ",lasjson["CURVES"])
-				// console.log("lasjson['CURVES'][curve_names_array_holder[counter_of_curve_names]] = ",lasjson["CURVES"][curve_names_array_holder[counter_of_curve_names]])
-				// console.log("curve_data_line_array[k] = ",curve_data_line_array[k], " and k = ",k)
-				// console.log("curve_names_array_holder[counter_of_curve_names] = ",curve_names_array_holder[counter_of_curve_names])
-				// console.log("lasjson['CURVES'][curve_names_array_holder[counter_of_curve_names]] = ",lasjson["CURVES"][curve_names_array_holder[counter_of_curve_names]])
-				// //lasjson["CURVES"][curve_names_array_holder[counter_of_curve_names]] = 
-				
+			if(curve_data_line_array[k] !== ""){				
 				lasjson["CURVES"][curve_names_array_holder[counter_of_curve_names]].push(curve_data_line_array[k])
 				counter_of_curve_names += 1;
-				// console.log("5b counter_of_curve_names = ",counter_of_curve_names)
 			}
 		}
 	}
-	
 	console.log(" test: lasjson",lasjson);
 	return(lasjson)
 }
