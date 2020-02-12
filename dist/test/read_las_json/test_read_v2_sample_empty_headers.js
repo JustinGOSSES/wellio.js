9fae805004878f93752449dd07ce1f949f2fc84d
JustinGOSSES <jgosses82@gmail.com>
2020-02-12 08:39:05 -0600

    test documentation

--- dist/test/read_las_json/test_read_v2_sample_empty_headers.js
+++ dist/test/read_las_json/test_read_v2_sample_empty_headers.js
@@ -1,6 +1,9 @@
 const test = require('tape');
 const wellio = require('../../index.js');
 
+/**
+* A test function for v2 empty headers
+*/
 test('readLasioJson: test_read_v2_sample_empty_headers', function(t) {
   t.plan(2);
   json_file = "assets/json_files/sample_2.0_empty_headers.json";
